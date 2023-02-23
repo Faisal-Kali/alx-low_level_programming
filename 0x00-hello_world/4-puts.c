945e6aadc7957973aa90d415c95ef45ab519f04b
Faisal Salisu <faisalsalisu51@gmail.com>
2023-02-23 03:37:47 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * A C program that prints an output and
- * returns the value zero
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
